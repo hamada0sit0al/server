6cac2b6c7a3ef02f65d2cd70cb8894558d1607a2
hamada0sit0al <al16082@shibaura-it.ac.jp>
2019-04-22 10:56:12 +0900

    IoTshisaku_ver1.0

--- server.py
+++ server.py
@@ -1,33 +1,46 @@
+import datetime
+import csv
 from flask import Flask, request, render_template
+name = './' + str(datetime.date.today())
+file_path = name + '.csv'
+
 app = Flask(__name__) 
-file_path = "./sensor_data.csv" 
+
 my_port = 16082 
  
 @app.route('/', methods=['GET']) 
 def get_html():   
     return render_template('./index.html')    
 
-@app.route('/lux', methods=['POST']) 
-def update_lux():   
+@app.route('/second', methods=['POST']) 
+def update_second():   
     time = request.form["time"]   
-    lux = request.form["lux"]   
+    second = request.form["second"]
+    print(second)   
     try:     
-        f = open(file_path, 'w')     
-        f.write(time + "," + lux)     
+        f = open(file_path, 'a')     
+        f.write(time + "," + second + "\n")     
         return "succeeded to write"   
-    except Exception as e:     
+    except Exception as e:
         print(e)     
         return "failed to write"   
     finally:     
         f.close() 
  
-@app.route('/lux', methods=['GET']) 
-def get_lux():   
+@app.route('/second', methods=['GET']) 
+def get_second():   
     try:     
         f = open(file_path, 'r')     
-        for row in f:       
-            lux = row     
-        return lux   
+        for row in f:        
+            second = row[17:]
+        print(type(second))
+        temp = int(second) / 3600
+        hour = str(int(temp))
+        temp = (int(second) / 60) - int(hour)*60 
+        minute = str(int(temp))
+        date = datetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+        data = date + " 現在<br>計 " + hour + " 時間 " + minute + " 分在席"    
+        return data   
     except Exception as e:     
         print(e)     
         return e   
